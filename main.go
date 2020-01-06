c9685d738025768d5c67323c2ab13c6827b83643
jon <jonluo11@gmail.com>
2020-01-06 15:25:41 -0500

    basic working example

--- main.go
+++ main.go
@@ -3,38 +3,58 @@ package main
 import (
 	"bufio"
 	"compress/gzip"
+	"encoding/csv"
+	"flag"
 	"fmt"
+	"io"
+	"log"
 	"os"
+	"runtime"
+	"runtime/pprof"
 	"strings"
+	"time"
 )
 
 type Variant struct {
-	Chr, Pos int
-	Ref, Alt string
-	Calls    Call
+	Chr, Pos         string
+	Ref, Alt, Var_ID string
+	Calls            []Call
 }
 
 type Call struct {
-	PT_ID, Var_ID, RNC string
-	GT, GQ, Ref, Alt   int
+	PT_ID, RNC             string
+	GT, GQ, AD_Ref, AD_Alt string
 }
 
-func ReadVCFgz(filename string) (*bufio.Reader, []string) {
+func CheckError(err error) bool {
+	if err != nil {
+		if err == io.EOF {
+			return true
+		}
+	} else {
+		fmt.Println(err)
+		os.Exit(0)
+	}
+	return false
+}
+
+func ReadVCFgz(filename string) (*bufio.Reader, *os.File, *gzip.Reader, []string) {
 	f, err := os.Open(filename)
 	if err != nil {
 		fmt.Println("File not found")
 		os.Exit(0)
 	}
-	defer f.Close()
+	//defer f.Close()
 
 	gz, err := gzip.NewReader(f)
 	if err != nil {
 		fmt.Println("Error reading file")
 		os.Exit(1)
 	}
-	defer gz.Close()
+	//defer gz.Close()
 
 	reader := bufio.NewReader(gz)
+
 	var sampleNames []string
 
 	for {
@@ -51,7 +71,7 @@ func ReadVCFgz(filename string) (*bufio.Reader, []string) {
 		}
 	}
 
-	return reader, sampleNames
+	return reader, f, gz, sampleNames
 }
 
 func ParseSampleNames(line string) []string {
@@ -66,7 +86,97 @@ func ParseSampleNames(line string) []string {
 
 // GT:DP:AD:SBPV:GQ:PL:FT:RNC
 
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
+var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
+
 func main() {
-	_, sampleNames := ReadVCFgz("PKD1_IDT_F145K.vcf.gz")
-	fmt.Println(sampleNames)
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		}
+		defer f.Close()
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
+		defer pprof.StopCPUProfile()
+	}
+	now := time.Now()
+	fmt.Println(now)
+	reader, f, gz, sampleNames := ReadVCFgz("PKD1_IDT_F145K.vcf.gz")
+	file, err := os.Create("testout.csv")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(5)
+	}
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+	defer f.Close()
+	defer gz.Close()
+	//defer file.Close()
+	//fmt.Println(sampleNames)
+	//line, _ := reader.ReadString('\n')
+	//line, _,_ := reader.ReadLine()
+	//fmt.Println(reader.ReadString('\n'))
+	//fmt.Println(reader.ReadString('\n'))
+	for {
+		line, err := reader.ReadString('\n')
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		row := strings.Split(line, "\t")
+		variant := Variant{
+			Chr:    row[0],
+			Pos:    row[1],
+			Ref:    row[3],
+			Alt:    row[4],
+			Var_ID: "hi",
+		}
+
+		for i, _ := range row[9:] {
+			//fmt.Println(i)
+			call := strings.Split(row[i+9], ":")
+			//fmt.Println(call)
+			if call[0] == "0/1" {
+				gt := strings.Split(call[2], ",")
+				variant.Calls = append(variant.Calls, Call{
+					PT_ID:  sampleNames[i],
+					RNC:    call[7],
+					GT:     call[0],
+					GQ:     call[4],
+					AD_Ref: gt[0],
+					AD_Alt: gt[1],
+				})
+				//fmt.Println(variant.Calls)
+			}
+		}
+		for i := range variant.Calls {
+			writer.Write([]string{variant.Calls[i].PT_ID,
+				variant.Calls[i].GT,
+				variant.Calls[i].GQ,
+				variant.Calls[i].AD_Ref,
+				variant.Calls[i].AD_Alt,
+				variant.Calls[i].RNC})
+		}
+	}
+
+	fmt.Println(time.Since(now))
+
+	if *memprofile != "" {
+		f, err := os.Create(*memprofile)
+		if err != nil {
+			log.Fatal("could not create memory profile: ", err)
+		}
+		defer f.Close()
+		runtime.GC() // get up-to-date statistics
+		if err := pprof.WriteHeapProfile(f); err != nil {
+			log.Fatal("could not write memory profile: ", err)
+		}
+	}
+
 }
