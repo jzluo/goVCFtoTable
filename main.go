c65a27c4dab8728ef44d5ae4c0f55a7e16f22456
jon <jonluo11@gmail.com>
2020-01-07 11:28:42 -0500

    process bytes instead of strings

--- main.go
+++ main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"compress/gzip"
-	"encoding/csv"
+	//"encoding/csv"
 	"flag"
 	"fmt"
 	"io"
@@ -11,19 +12,19 @@ import (
 	"os"
 	"runtime"
 	"runtime/pprof"
-	"strings"
+	//"strings"
 	"time"
 )
 
 type Variant struct {
-	Chr, Pos         string
-	Ref, Alt, Var_ID string
+	Chr, Pos         []byte
+	Ref, Alt, Var_ID []byte
 	Calls            []Call
 }
 
 type Call struct {
-	PT_ID, RNC             string
-	GT, GQ, AD_Ref, AD_Alt string
+	PT_ID, RNC             []byte
+	GT, GQ, AD_Ref, AD_Alt []byte
 }
 
 func CheckError(err error) bool {
@@ -38,7 +39,8 @@ func CheckError(err error) bool {
 	return false
 }
 
-func ReadVCFgz(filename string) (*bufio.Reader, *os.File, *gzip.Reader, []string) {
+//func ReadVCFgz(filename string) (*bufio.Reader, *os.File, *gzip.Reader, []string) {
+func ReadVCFgz(filename string) (*bufio.Reader, *os.File, *gzip.Reader, [][]byte) {
 	f, err := os.Open(filename)
 	if err != nil {
 		fmt.Println("File not found")
@@ -55,17 +57,19 @@ func ReadVCFgz(filename string) (*bufio.Reader, *os.File, *gzip.Reader, []string
 
 	reader := bufio.NewReader(gz)
 
-	var sampleNames []string
+	var sampleNames [][]byte
 
 	for {
-		line, err := reader.ReadString('\n')
+		//line, err := reader.ReadString('\n')
+		line, err := reader.ReadBytes('\n')
 		if err != nil {
 			fmt.Println("Error parsing VCF file")
 			fmt.Println(err)
 			os.Exit(3)
 		}
 
-		if strings.HasPrefix(line, "#CHROM") {
+		//if strings.HasPrefix(line, "#CHROM") {
+		if bytes.HasPrefix(line, []byte("#CHROM")) {
 			sampleNames = ParseSampleNames(line)
 			break
 		}
@@ -74,14 +78,88 @@ func ReadVCFgz(filename string) (*bufio.Reader, *os.File, *gzip.Reader, []string
 	return reader, f, gz, sampleNames
 }
 
-func ParseSampleNames(line string) []string {
-	var sampleNames []string
-	splitLine := strings.Split(line, "\t")
+//func ParseSampleNames(line string) []string {
+//var sampleNames []string
+//splitLine := strings.Split(line, "\t")
+//sampleNames = splitLine[9:]
+//for i := range sampleNames {
+//	sampleNames[i] = strings.Split(sampleNames[i], "_")[1]
+//}
+//return sampleNames
+//}
+func ParseSampleNames(line []byte) [][]byte {
+	var sampleNames [][]byte
+	splitLine := bytes.Split(line, []byte("\t"))
 	sampleNames = splitLine[9:]
 	for i := range sampleNames {
-		sampleNames[i] = strings.Split(sampleNames[i], "_")[1]
+		sampleNames[i] = bytes.Split(sampleNames[i], []byte("_"))[1]
 	}
 	return sampleNames
+}
+
+func GetFields(call []byte) ([]byte, []byte, []byte, []byte) {
+	//for {
+	//	if bytes.IndexByte(call[4:], delim) == -1 {
+	//
+	//	}
+	//	bytes.IndexByte(call[start])
+	//}
+
+	// get AD
+	//fmt.Println("call", string(call))
+
+	call = call[4:]
+	end := bytes.IndexByte(call, ':')
+	//end = bytes.IndexByte(call[4+start:], ',') + 4
+	call = call[end+1:]
+	end = bytes.IndexByte(call, ',')
+	if end == -1 {
+		return []byte("h"), []byte("h"), []byte("h"), []byte("h")
+	}
+	ref := call[:end]
+	//fmt.Println("ref", string(ref))
+
+	call = call[end+1:]
+	end = bytes.IndexByte(call, ':')
+	alt := call[:end]
+	//fmt.Println("alt", string(alt))
+
+	call = call[end+1:]
+	//start = bytes.IndexByte(call[end:], ':') // SBPV end
+	end = bytes.IndexByte(call, ':')
+	call = call[end+1:] // skip SBPV
+	end = bytes.IndexByte(call, ':')
+	gq := call[:end]
+	//fmt.Println("gq", string(gq))
+
+	call = call[end+1:]
+	end = bytes.IndexByte(call, ':')
+	call = call[end+1:] // skip PL
+	end = bytes.IndexByte(call, ':')
+	call = call[end+1:]
+	end = bytes.IndexByte(call, ':')
+	call = call[end+1:] // skip FT
+	//start = bytes.IndexByte(call[end:], ':') // PL end
+	//end = bytes.IndexByte(call, ':')
+	//start = bytes.IndexByte(call[end:], ':') // FT end
+	rnc := call
+	//fmt.Println("rnc", string(rnc))
+
+	//t := bytes.Split(call, []byte(":"))
+	//if len(t[2]) == 1 {
+	//	return t[4], t[4], t[4], t[4]
+	//}
+	//ad := bytes.Split(t[2], []byte(","))
+	////fmt.Println(string(call))
+	//ref := ad[0]
+	//alt := ad[1]
+	//gq := t[4]
+	//rnc := t[7]
+	return ref, alt, gq, rnc
+}
+
+func SplitRow(row []byte) [][]byte {
+
 }
 
 // GT:DP:AD:SBPV:GQ:PL:FT:RNC
@@ -110,7 +188,8 @@ func main() {
 		fmt.Println(err)
 		os.Exit(5)
 	}
-	writer := csv.NewWriter(file)
+	//writer := csv.NewWriter(file)
+	writer := bufio.NewWriter(file)
 	defer writer.Flush()
 	defer f.Close()
 	defer gz.Close()
@@ -120,8 +199,10 @@ func main() {
 	//line, _,_ := reader.ReadLine()
 	//fmt.Println(reader.ReadString('\n'))
 	//fmt.Println(reader.ReadString('\n'))
+	het := []byte("0/1")
 	for {
-		line, err := reader.ReadString('\n')
+		//line, err := reader.ReadString('\n')
+		line, err := reader.ReadBytes('\n')
 		if err == io.EOF {
 			break
 		} else if err != nil {
@@ -129,39 +210,49 @@ func main() {
 			os.Exit(1)
 		}
 
-		row := strings.Split(line, "\t")
+		//row := strings.Split(line, "\t")
+		row := bytes.Split(line, []byte("\t"))
 		variant := Variant{
 			Chr:    row[0],
 			Pos:    row[1],
 			Ref:    row[3],
 			Alt:    row[4],
-			Var_ID: "hi",
+			Var_ID: []byte("hi"),
 		}
 
-		for i, _ := range row[9:] {
+		for i, call := range row[9:] {
 			//fmt.Println(i)
-			call := strings.Split(row[i+9], ":")
+			//call := strings.Split(row[i+9], ":")
+			//call := bytes.Split(row[i+9], []byte(":"))
+
 			//fmt.Println(call)
-			if call[0] == "0/1" {
-				gt := strings.Split(call[2], ",")
+			//if call[0] == "0/1" {
+			//if bytes.Equal(call[0:3], []byte("0/1")) {
+			if call[2] == 49 {
+				ref, alt, gq, rnc := GetFields(call)
+				//gt := strings.Split(call[2], ",")
+				//gt := bytes.Split(call[2], []byte(","))
 				variant.Calls = append(variant.Calls, Call{
 					PT_ID:  sampleNames[i],
-					RNC:    call[7],
-					GT:     call[0],
-					GQ:     call[4],
-					AD_Ref: gt[0],
-					AD_Alt: gt[1],
+					RNC:    rnc,
+					GT:     het,
+					GQ:     gq,
+					AD_Ref: ref,
+					AD_Alt: alt,
 				})
 				//fmt.Println(variant.Calls)
 			}
 		}
 		for i := range variant.Calls {
-			writer.Write([]string{variant.Calls[i].PT_ID,
+			//writer.Write([]string{variant.Calls[i].PT_ID,
+			writer.Write(bytes.Join([][]byte{
 				variant.Calls[i].GT,
 				variant.Calls[i].GQ,
 				variant.Calls[i].AD_Ref,
 				variant.Calls[i].AD_Alt,
-				variant.Calls[i].RNC})
+				variant.Calls[i].RNC,
+			}, []byte(",")))
+			writer.Write([]byte{'\n'})
 		}
 	}
 
