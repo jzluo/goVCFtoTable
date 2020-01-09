dd4c578bafec1873afc3947718c961c1741fc132
jon <jonluo11@gmail.com>
2020-01-09 18:56:05 -0500

    use bgzf for parallel decompression (github.com/biogo/hts/bgzf), cleaned up code

--- main.go
+++ main.go
@@ -3,29 +3,16 @@ package main
 import (
 	"bufio"
 	"bytes"
-	"compress/gzip"
-	//"encoding/csv"
 	"flag"
 	"fmt"
+	"github.com/biogo/hts/bgzf"
 	"io"
 	"log"
 	"os"
 	"runtime"
 	"runtime/pprof"
-	//"strings"
 	"time"
 )
-
-type Variant struct {
-	Chr, Pos         []byte
-	Ref, Alt, Var_ID []byte
-	Calls            []Call
-}
-
-type Call struct {
-	PT_ID, RNC             []byte
-	GT, GQ, AD_Ref, AD_Alt []byte
-}
 
 func CheckError(err error) bool {
 	if err != nil {
@@ -40,27 +27,22 @@ func CheckError(err error) bool {
 }
 
 //func ReadVCFgz(filename string) (*bufio.Reader, *os.File, *gzip.Reader, []string) {
-func ReadVCFgz(filename string) (*bufio.Reader, *os.File, *gzip.Reader, [][]byte) {
+func ReadVCFgz(filename string) (*bufio.Reader, *os.File, *bgzf.Reader, [][]byte, int) {
 	f, err := os.Open(filename)
 	if err != nil {
 		fmt.Println("File not found")
 		os.Exit(0)
 	}
-	//defer f.Close()
 
-	gz, err := gzip.NewReader(f)
+	gz, err := bgzf.NewReader(f, 8)
 	if err != nil {
 		fmt.Println("Error reading file")
 		os.Exit(1)
 	}
-	//defer gz.Close()
 
 	reader := bufio.NewReader(gz)
 
-	var sampleNames [][]byte
-
 	for {
-		//line, err := reader.ReadString('\n')
 		line, err := reader.ReadBytes('\n')
 		if err != nil {
 			fmt.Println("Error parsing VCF file")
@@ -68,101 +50,128 @@ func ReadVCFgz(filename string) (*bufio.Reader, *os.File, *gzip.Reader, [][]byte
 			os.Exit(3)
 		}
 
-		//if strings.HasPrefix(line, "#CHROM") {
 		if bytes.HasPrefix(line, []byte("#CHROM")) {
-			sampleNames = ParseSampleNames(line)
-			break
+			sampleNames, numSamples := ParseSampleNames(line)
+			return reader, f, gz, sampleNames, numSamples
 		}
 	}
-
-	return reader, f, gz, sampleNames
 }
 
-//func ParseSampleNames(line string) []string {
-//var sampleNames []string
-//splitLine := strings.Split(line, "\t")
-//sampleNames = splitLine[9:]
-//for i := range sampleNames {
-//	sampleNames[i] = strings.Split(sampleNames[i], "_")[1]
-//}
-//return sampleNames
-//}
-func ParseSampleNames(line []byte) [][]byte {
-	var sampleNames [][]byte
-	splitLine := bytes.Split(line, []byte("\t"))
-	sampleNames = splitLine[9:]
-	for i := range sampleNames {
-		sampleNames[i] = bytes.Split(sampleNames[i], []byte("_"))[1]
+func ParseSampleNames(line []byte) ([][]byte, int) {
+	sampleNames := make([][]byte, 0)
+	//splitLine := bytes.Split(line, []byte("\t"))
+	var start int
+	_, start = GetNextCol(line, start, 9, '\t')
+
+	for start > 0 { //GetNextCol() returns 0 when there are no more delimiters
+		var sample []byte
+		sample, start = GetNextCol(line, start, 0, '\t')
+		sampleNames = append(sampleNames, sample)
 	}
-	return sampleNames
+
+	return sampleNames, len(sampleNames)
 }
 
-func GetFields(call []byte) ([]byte, []byte, []byte, []byte) {
-	//for {
-	//	if bytes.IndexByte(call[4:], delim) == -1 {
-	//
-	//	}
-	//	bytes.IndexByte(call[start])
-	//}
-
-	// get AD
-	//fmt.Println("call", string(call))
-
-	call = call[4:]
-	end := bytes.IndexByte(call, ':')
-	//end = bytes.IndexByte(call[4+start:], ',') + 4
-	call = call[end+1:]
-	end = bytes.IndexByte(call, ',')
-	if end == -1 {
-		return []byte("h"), []byte("h"), []byte("h"), []byte("h")
+func GetNextCol(row []byte, start int, skip int, delim byte) ([]byte, int) {
+	for i := 0; i < skip; i++ {
+		start = bytes.IndexByte(row[start:], delim) + start + 1
 	}
-	ref := call[:end]
-	//fmt.Println("ref", string(ref))
-
-	call = call[end+1:]
-	end = bytes.IndexByte(call, ':')
-	alt := call[:end]
-	//fmt.Println("alt", string(alt))
-
-	call = call[end+1:]
-	//start = bytes.IndexByte(call[end:], ':') // SBPV end
-	end = bytes.IndexByte(call, ':')
-	call = call[end+1:] // skip SBPV
-	end = bytes.IndexByte(call, ':')
-	gq := call[:end]
-	//fmt.Println("gq", string(gq))
-
-	call = call[end+1:]
-	end = bytes.IndexByte(call, ':')
-	call = call[end+1:] // skip PL
-	end = bytes.IndexByte(call, ':')
-	call = call[end+1:]
-	end = bytes.IndexByte(call, ':')
-	call = call[end+1:] // skip FT
-	//start = bytes.IndexByte(call[end:], ':') // PL end
-	//end = bytes.IndexByte(call, ':')
-	//start = bytes.IndexByte(call[end:], ':') // FT end
-	rnc := call
-	//fmt.Println("rnc", string(rnc))
-
-	//t := bytes.Split(call, []byte(":"))
-	//if len(t[2]) == 1 {
-	//	return t[4], t[4], t[4], t[4]
-	//}
-	//ad := bytes.Split(t[2], []byte(","))
-	////fmt.Println(string(call))
-	//ref := ad[0]
-	//alt := ad[1]
-	//gq := t[4]
-	//rnc := t[7]
-	return ref, alt, gq, rnc
+	nextTabPos := bytes.IndexByte(row[start:], delim)
+	if nextTabPos == -1 {
+		return row[start:], 0
+	}
+	return row[start : start+nextTabPos], start + nextTabPos + 1
 }
 
-func SplitRow(row []byte) [][]byte {
-
+func GetReads(ad []byte) ([]byte, []byte) {
+	split := bytes.IndexByte(ad, ',')
+	return ad[:split], ad[split+1:]
 }
 
-// GT:DP:AD:SBPV:GQ:PL:FT:RNC
+func GetFormatFields(call []byte) ([]byte, []byte, []byte, []byte, []byte) {
+	// GT:DP:AD:SBPV:GQ:PL:FT:RNC
+	var dp, ad, ad_ref, ad_alt, gq, ft []byte
+	var start int
+	dp, start = GetNextCol(call, 4, 0, ':')
+	ad, start = GetNextCol(call, start, 0, ':')
+	ad_ref, ad_alt = GetReads(ad)
+	gq, start = GetNextCol(call, start, 1, ':')
+	ft, start = GetNextCol(call, start, 1, ':')
+	if ft[0] == 46 { // if ft=.
+		ft = []byte("")
+	}
+
+	return dp, ad_ref, ad_alt, gq, ft
+}
+
+func VCFtoTable(reader *bufio.Reader, writer *bufio.Writer, sampleNames [][]byte, numSamples int) {
+	for {
+		line, err := reader.ReadBytes('\n')
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		ProcessVariant(line, writer, sampleNames, numSamples)
+	}
+}
+
+func ProcessVariant(row []byte, writer *bufio.Writer, sampleNames [][]byte, numSamples int) {
+	//var chr, pos, ref, alt []byte
+	//var start int
+	//chr, start = GetNextCol(row, 0, 0, '\t')
+	//pos, start = GetNextCol(row, start, 0, '\t')
+	//ref, start = GetNextCol(row, start, 1, '\t')
+	//alt, start = GetNextCol(row, start, 0, '\t')
+	_, start := GetNextCol(row, 0, 9, '\t')
+	ProcessCalls(row[start:], writer, sampleNames, numSamples)
+}
+
+func ProcessCalls(calls []byte, writer *bufio.Writer, sampleNames [][]byte, numSamples int) {
+	var start int
+	het := []byte("0/1")
+	hetcmp := []byte("compound")
+	hom := []byte("1/1")
+	com := []byte(",")
+	newline := []byte("\n")
+	for i := 0; i < numSamples; i++ {
+		var call, flag, gt []byte
+		call, start = GetNextCol(calls, start, 0, '\t')
+
+		if call[0] == '.' || call[2] == '.' {
+			continue
+		}
+
+		if call[0] == 48 && call[2] == 49 {
+			gt = het
+		} else if call[0] == 49 && call[2] == 49 {
+			gt = hom
+		} else if call[0] == 49 && call[2] == 48 {
+			gt = het
+			flag = hetcmp
+		} else if call[0] == 48 && call[2] == 48 {
+			continue
+		} else {
+			fmt.Println(string(call[:3]))
+			panic("wtfff")
+		}
+		dp, ad_ref, ad_alt, gq, ft := GetFormatFields(call)
+
+		writer.Write(bytes.Join([][]byte{
+			sampleNames[i],
+			gt,
+			gq,
+			ad_ref,
+			ad_alt,
+			dp,
+			ft,
+			flag,
+			newline,
+		}, com))
+	}
+}
 
 var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
 var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
@@ -182,79 +191,19 @@ func main() {
 	}
 	now := time.Now()
 	fmt.Println(now)
-	reader, f, gz, sampleNames := ReadVCFgz("PKD1_IDT_F145K.vcf.gz")
-	file, err := os.Create("testout.csv")
+	reader, f, gz, sampleNames, numSamples := ReadVCFgz("PKD1_IDT_F145K.vcf.gz")
+	file, err := os.Create("testout_fancy.csv")
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(5)
 	}
-	//writer := csv.NewWriter(file)
+
 	writer := bufio.NewWriter(file)
 	defer writer.Flush()
 	defer f.Close()
 	defer gz.Close()
-	//defer file.Close()
-	//fmt.Println(sampleNames)
-	//line, _ := reader.ReadString('\n')
-	//line, _,_ := reader.ReadLine()
-	//fmt.Println(reader.ReadString('\n'))
-	//fmt.Println(reader.ReadString('\n'))
-	het := []byte("0/1")
-	for {
-		//line, err := reader.ReadString('\n')
-		line, err := reader.ReadBytes('\n')
-		if err == io.EOF {
-			break
-		} else if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
 
-		//row := strings.Split(line, "\t")
-		row := bytes.Split(line, []byte("\t"))
-		variant := Variant{
-			Chr:    row[0],
-			Pos:    row[1],
-			Ref:    row[3],
-			Alt:    row[4],
-			Var_ID: []byte("hi"),
-		}
-
-		for i, call := range row[9:] {
-			//fmt.Println(i)
-			//call := strings.Split(row[i+9], ":")
-			//call := bytes.Split(row[i+9], []byte(":"))
-
-			//fmt.Println(call)
-			//if call[0] == "0/1" {
-			//if bytes.Equal(call[0:3], []byte("0/1")) {
-			if call[2] == 49 {
-				ref, alt, gq, rnc := GetFields(call)
-				//gt := strings.Split(call[2], ",")
-				//gt := bytes.Split(call[2], []byte(","))
-				variant.Calls = append(variant.Calls, Call{
-					PT_ID:  sampleNames[i],
-					RNC:    rnc,
-					GT:     het,
-					GQ:     gq,
-					AD_Ref: ref,
-					AD_Alt: alt,
-				})
-				//fmt.Println(variant.Calls)
-			}
-		}
-		for i := range variant.Calls {
-			//writer.Write([]string{variant.Calls[i].PT_ID,
-			writer.Write(bytes.Join([][]byte{
-				variant.Calls[i].GT,
-				variant.Calls[i].GQ,
-				variant.Calls[i].AD_Ref,
-				variant.Calls[i].AD_Alt,
-				variant.Calls[i].RNC,
-			}, []byte(",")))
-			writer.Write([]byte{'\n'})
-		}
-	}
+	VCFtoTable(reader, writer, sampleNames, numSamples)
 
 	fmt.Println(time.Since(now))
 
