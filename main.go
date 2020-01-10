619598b9a6325c21cabe6141e6c537c75c433d36
jon <jonluo11@gmail.com>
2020-01-10 17:32:45 -0500

    added os args for filenames and number of threads for bgzf decompression

--- main.go
+++ main.go
@@ -3,38 +3,69 @@ package main
 import (
 	"bufio"
 	"bytes"
-	"flag"
+	"strconv"
+	//"flag"
 	"fmt"
 	"github.com/biogo/hts/bgzf"
 	"io"
-	"log"
+	//"log"
 	"os"
-	"runtime"
-	"runtime/pprof"
+	//"runtime"
+	//"runtime/pprof"
 	"time"
 )
 
-func CheckError(err error) bool {
+//var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
+//var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
+
+func main() {
+	//flag.Parse()
+	//if *cpuprofile != "" {
+	//	f, err := os.Create(*cpuprofile)
+	//	if err != nil {
+	//		log.Fatal("could not create CPU profile: ", err)
+	//	}
+	//	defer f.Close()
+	//	if err := pprof.StartCPUProfile(f); err != nil {
+	//		log.Fatal("could not start CPU profile: ", err)
+	//	}
+	//	defer pprof.StopCPUProfile()
+	//}
+	now := time.Now()
+	fileName := os.Args[1]
+	outFile := os.Args[2]
+	fmt.Println(now, "Doing", fileName)
+	threads, err := strconv.Atoi(os.Args[3])
 	if err != nil {
-		if err == io.EOF {
-			return true
-		}
-	} else {
-		fmt.Println(err)
-		os.Exit(0)
+		fmt.Println("invalid number of threads")
+		os.Exit(9)
 	}
-	return false
+
+	VCFtoTable(fileName, outFile, threads)
+
+	fmt.Println("Finished", fileName, "in", time.Since(now))
+
+	//if *memprofile != "" {
+	//	f, err := os.Create(*memprofile)
+	//	if err != nil {
+	//		log.Fatal("could not create memory profile: ", err)
+	//	}
+	//	defer f.Close()
+	//	runtime.GC() // get up-to-date statistics
+	//	if err := pprof.WriteHeapProfile(f); err != nil {
+	//		log.Fatal("could not write memory profile: ", err)
+	//	}
+	//}
 }
 
-//func ReadVCFgz(filename string) (*bufio.Reader, *os.File, *gzip.Reader, []string) {
-func ReadVCFgz(filename string) (*bufio.Reader, *os.File, *bgzf.Reader, [][]byte, int) {
+func ReadVCFgz(filename string, threads int) (*bufio.Reader, *os.File, *bgzf.Reader, [][]byte, int) {
 	f, err := os.Open(filename)
 	if err != nil {
 		fmt.Println("File not found")
 		os.Exit(0)
 	}
 
-	gz, err := bgzf.NewReader(f, 8)
+	gz, err := bgzf.NewReader(f, threads)
 	if err != nil {
 		fmt.Println("Error reading file")
 		os.Exit(1)
@@ -57,9 +88,106 @@ func ReadVCFgz(filename string) (*bufio.Reader, *os.File, *bgzf.Reader, [][]byte
 	}
 }
 
+func VCFtoTable(fileName, outFile string, threads int) {
+
+	reader, f, gz, sampleNames, numSamples := ReadVCFgz(fileName, threads)
+	file, err := os.Create(outFile)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(5)
+	}
+
+	writer := bufio.NewWriter(file)
+	defer writer.Flush()
+	defer f.Close()
+	defer gz.Close()
+
+	writer.WriteString("Var_ID\tPT_ID\tGT\tGQ\tAD_REF\tAD_ALT\tDP\tFT\tCompound\n")
+
+	for {
+		line, err := reader.ReadBytes('\n')
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		ProcessVariant(line, writer, sampleNames, numSamples)
+	}
+}
+
+func ProcessVariant(row []byte, writer *bufio.Writer, sampleNames [][]byte, numSamples int) {
+	var varID []byte
+	var start int
+	//chr, start = GetNextCol(row, 0, 0, '\t')
+	//pos, start = GetNextCol(row, start, 0, '\t')
+	varID, start = GetNextCol(row, start, 2, '\t')
+	//ref, start = GetNextCol(row, start, 0, '\t')
+	//alt, start = GetNextCol(row, start, 0, '\t')
+	_, start = GetNextCol(row, 0, 9, '\t')
+	ProcessCalls(row[start:], writer, sampleNames, numSamples, varID)
+}
+
+func ProcessCalls(calls []byte, writer *bufio.Writer, sampleNames [][]byte, numSamples int, varID []byte) {
+	var start int
+	het := []byte{49}
+	cmp := []byte("CMPND")
+	hom := []byte{50}
+	// comma := []byte(",")
+	tab := []byte("\t")
+	newline := []byte("\n")
+	zero := []byte{48}
+
+	for i := 0; i < numSamples; i++ {
+		var call, gt []byte
+		var compound bool
+		call, start = GetNextCol(calls, start, 0, '\t')
+
+		if call[0] == 48 && call[2] == 48 { // 0/0
+			continue
+		} else if call[0] == '.' || call[2] == '.' {
+			continue
+		} else if call[0] == 48 && call[2] == 49 { // 0/1
+			gt = het
+		} else if call[0] == 49 && call[2] == 49 { // 1/1
+			gt = hom
+		} else if call[0] == 49 && call[2] == 48 { // 1/0
+			gt = het
+			compound = true
+		} else {
+			fmt.Println(string(call[:3]))
+			panic("wtf is this")
+			os.Exit(10)
+		}
+
+		dp, ad_ref, ad_alt, gq, ft := GetFormatFields(call)
+		if ad_ref[0] == '.' {
+			ad_ref = zero
+		}
+		if compound {
+			if len(ft) == 0 {
+				ft = cmp
+			} else if len(ft) > 0 {
+				ft = append(ft, ";CMPND"...)
+			}
+		}
+
+		writer.Write(bytes.Join([][]byte{
+			varID,
+			sampleNames[i],
+			gt,
+			gq,
+			ad_ref,
+			ad_alt,
+			dp,
+			ft,
+		}, tab))
+		writer.Write(newline)
+	}
+}
+
 func ParseSampleNames(line []byte) ([][]byte, int) {
 	sampleNames := make([][]byte, 0)
-	//splitLine := bytes.Split(line, []byte("\t"))
 	var start int
 	_, start = GetNextCol(line, start, 9, '\t')
 
@@ -102,121 +230,4 @@ func GetFormatFields(call []byte) ([]byte, []byte, []byte, []byte, []byte) {
 	}
 
 	return dp, ad_ref, ad_alt, gq, ft
-}
-
-func VCFtoTable(reader *bufio.Reader, writer *bufio.Writer, sampleNames [][]byte, numSamples int) {
-	for {
-		line, err := reader.ReadBytes('\n')
-		if err == io.EOF {
-			break
-		} else if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		ProcessVariant(line, writer, sampleNames, numSamples)
-	}
-}
-
-func ProcessVariant(row []byte, writer *bufio.Writer, sampleNames [][]byte, numSamples int) {
-	//var chr, pos, ref, alt []byte
-	//var start int
-	//chr, start = GetNextCol(row, 0, 0, '\t')
-	//pos, start = GetNextCol(row, start, 0, '\t')
-	//ref, start = GetNextCol(row, start, 1, '\t')
-	//alt, start = GetNextCol(row, start, 0, '\t')
-	_, start := GetNextCol(row, 0, 9, '\t')
-	ProcessCalls(row[start:], writer, sampleNames, numSamples)
-}
-
-func ProcessCalls(calls []byte, writer *bufio.Writer, sampleNames [][]byte, numSamples int) {
-	var start int
-	het := []byte("0/1")
-	hetcmp := []byte("compound")
-	hom := []byte("1/1")
-	com := []byte(",")
-	newline := []byte("\n")
-	for i := 0; i < numSamples; i++ {
-		var call, flag, gt []byte
-		call, start = GetNextCol(calls, start, 0, '\t')
-
-		if call[0] == '.' || call[2] == '.' {
-			continue
-		}
-
-		if call[0] == 48 && call[2] == 49 {
-			gt = het
-		} else if call[0] == 49 && call[2] == 49 {
-			gt = hom
-		} else if call[0] == 49 && call[2] == 48 {
-			gt = het
-			flag = hetcmp
-		} else if call[0] == 48 && call[2] == 48 {
-			continue
-		} else {
-			fmt.Println(string(call[:3]))
-			panic("wtfff")
-		}
-		dp, ad_ref, ad_alt, gq, ft := GetFormatFields(call)
-
-		writer.Write(bytes.Join([][]byte{
-			sampleNames[i],
-			gt,
-			gq,
-			ad_ref,
-			ad_alt,
-			dp,
-			ft,
-			flag,
-			newline,
-		}, com))
-	}
-}
-
-var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
-var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
-
-func main() {
-	flag.Parse()
-	if *cpuprofile != "" {
-		f, err := os.Create(*cpuprofile)
-		if err != nil {
-			log.Fatal("could not create CPU profile: ", err)
-		}
-		defer f.Close()
-		if err := pprof.StartCPUProfile(f); err != nil {
-			log.Fatal("could not start CPU profile: ", err)
-		}
-		defer pprof.StopCPUProfile()
-	}
-	now := time.Now()
-	fmt.Println(now)
-	reader, f, gz, sampleNames, numSamples := ReadVCFgz("PKD1_IDT_F145K.vcf.gz")
-	file, err := os.Create("testout_fancy.csv")
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(5)
-	}
-
-	writer := bufio.NewWriter(file)
-	defer writer.Flush()
-	defer f.Close()
-	defer gz.Close()
-
-	VCFtoTable(reader, writer, sampleNames, numSamples)
-
-	fmt.Println(time.Since(now))
-
-	if *memprofile != "" {
-		f, err := os.Create(*memprofile)
-		if err != nil {
-			log.Fatal("could not create memory profile: ", err)
-		}
-		defer f.Close()
-		runtime.GC() // get up-to-date statistics
-		if err := pprof.WriteHeapProfile(f); err != nil {
-			log.Fatal("could not write memory profile: ", err)
-		}
-	}
-
 }
